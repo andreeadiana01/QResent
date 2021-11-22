4704e8b5d92dcd638d0cb60fd959d5dcc29dd4e5
Diana <diana.apostu@stud.acs.upb.ro>
2021-11-22 22:32:33 +0200

    Generate pdf file

---
+++ client/generate.js
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/", {
+    waitUntil: 'networkidle0'
+  });
+  await page.setViewport({ width: 1680, height: 1050 });
+  await page.pdf({
+    path: "listaPrezenta.pdf",
+    format: "A4"
+  });
+
+  await browser.close();
+})();
